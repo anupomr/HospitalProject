060453fecaa778ee59e4f035973aae3772aa831b
Anupom Roy <anupomr@yahoo.com>
2019-04-16 17:08:53 -0400

    Created API base

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,57 +3,11 @@ let router = express.Router();
 let mongoose = require("mongoose");
 let passport = require("passport");
 
+let jwt=require('jsonwebtoken');
+let DB=require('../config/db')
 // define the User Model
 let userModel = require("../models/user");
 let User = userModel.User; // alias
-
-module.exports.displayHomePage = (req, res, next) => {
-  res.render("index", {
-    title: "Home",
-    displayName: req.user ? req.user.displayName : ""
-  });
-};
-
-module.exports.displayAboutPage = (req, res, next) => {
-  res.render("index", {
-    title: "About",
-    displayName: req.user ? req.user.displayName : ""
-  });
-};
-
-module.exports.displayProductsPage = (req, res, next) => {
-  res.render("index", {
-    title: "Products",
-    displayName: req.user ? req.user.displayName : ""
-  });
-};
-
-module.exports.displayServicesPage = (req, res, next) => {
-  res.render("index", {
-    title: "Services",
-    displayName: req.user ? req.user.displayName : ""
-  });
-};
-
-module.exports.displayContactPage = (req, res, next) => {
-  res.render("index", {
-    title: "Contact",
-    displayName: req.user ? req.user.displayName : ""
-  });
-};
-
-module.exports.displayLoginPage = (req, res, next) => {
-  // check if user is already logged in
-  if (!req.user) {
-    res.render("auth/login", {
-      title: "Login",
-      messages: req.flash("loginMessage"),
-      displayName: req.user ? req.user.displayName : ""
-    });
-  } else {
-    return res.redirect("/");
-  }
-};
 
 module.exports.processLoginPage = (req, res, next) => {
   passport.authenticate('local', 
@@ -64,30 +18,37 @@ module.exports.processLoginPage = (req, res, next) => {
     }
     // is there a user login error?
     if(!user) {
-      req.flash("loginMessage", "Authentication Error");
-      return res.redirect('/login');
+      
+      return res.json({success: false, msg: 'ERROR: Failed to Log In User!'});
     }
     req.logIn(user, (err) => {
       // server error?
       if(err) {
         return next(err);
       }
-      return res.redirect('/contact-list');
+
+      const payload = {
+        id: user._id,
+        displayName: user.displayName,
+        username: user.username,
+        email: user.email
+      }
+
+      const authToken = jwt.sign(payload, DB.secret, {
+        expiresIn: 604800 // 1 Week
+      });
+
+      return res.json({success: true, msg: 'User Logged in Successfully!', user: {
+        id: user._id,
+        displayName: user.displayName,
+        username: user.username,
+        email: user.email
+      }, token: authToken});
+
+
     });
   })(req, res, next);
 }
-
-module.exports.displayRegisterPage = (req, res, next) => {
-  if (!req.user) {
-    res.render("auth/register", {
-      title: "Register",
-      messages: req.flash("registerMessage"),
-      displayName: req.user ? req.user.displayName : ""
-    });
-  } else {
-    return res.redirect("/");
-  }
-};
 
 module.exports.processRegisterPage = (req, res, next) => {
   // define a new user object
@@ -108,23 +69,16 @@ module.exports.processRegisterPage = (req, res, next) => {
         );
         console.log("Error: User Already Exists!");
       }
-      return res.render("auth/register", {
-        title: "Register",
-        messages: req.flash("registerMessage"),
-        displayName: req.user ? req.user.displayName : ""
-      });
+      return res.json({success: false, msg: 'ERROR: Failed to Register User!'});
     } else {
       // if no error exists, then registration is successful
 
-      // redirect the user
-      return passport.authenticate("local")(req, res, () => {
-        res.redirect("/contact-list");
-      });
+      return res.json({success: true, msg: 'User Registered Successfully!'});
     }
   });
 };
 
 module.exports.performLogout = (req, res, next) => {
   req.logout();
-  res.redirect("/");
+  res.json({success: true, msg: 'User Successfully Logged out!'});
 };
